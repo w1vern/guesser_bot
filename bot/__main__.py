179d06beb1cfee3525ed8d7ae1646e6220b6eafd
w1vern <bndvvl@gmail.com>
2025-05-01 22:04:03 +0300

    upd

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,16 @@
+
+
 import asyncio
+
+from enum import Enum
 import math
-import random
 from uuid import UUID
 
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, BufferedInputFile
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, BufferedInputFile, ReplyKeyboardMarkup, KeyboardButton
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -18,38 +21,47 @@ from db.repositories.battle_repository import BattleRepository
 from db.repositories.question_repository import QuestionRepository
 from db.repositories.user_repository import UserRepository
 from db.s3 import get_s3_client
-from typing import Tuple, List
+import math
+from typing import Protocol, Tuple
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# FSM-состояния
-# —————————————————————————————————————————————————————————————————————————————
-class AppState(StatesGroup):
-    main_menu = State()
-    playing = State()
-    settings = State()
-    # можно добавить другие состояния (редактирование контента и т.п.)
-
-
-# —————————————————————————————————————————————————————————————————————————————
-# Помощники для работы с рейтингом (ваш существующий код)
-# —————————————————————————————————————————————————————————————————————————————
 def change_rank(user_rank: float, question_rank: float, result: bool) -> Tuple[float, float]:
     k = 5.0
     alpha = 0.1
+
     p_user = 1 / (1 + math.exp(k * (question_rank - user_rank)))
+
     tmp = int(result)
+
     user_diff = alpha * (tmp - p_user) * user_rank * (1 - user_rank)
-    question_diff = alpha * (p_user - tmp) * question_rank * (1 - question_rank)
+    question_diff = alpha * (p_user - tmp) * \
+        question_rank * (1 - question_rank)
+
     return user_diff, question_diff
 
+
+async def get_user(tg_id: int, session: AsyncSession) -> User:
+    ur = UserRepository(session)
+    user = await ur.get_by_tg_id(tg_id)
+    if user:
+        return user
+    raise Exception("something 3")
+
+
 def convert_rank(rank: float) -> int:
-    return int(rank * 1000)
+    return int(rank*1000)
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Менеджер сессий БД
-# —————————————————————————————————————————————————————————————————————————————
+async def register_user(tg_id: int, session: AsyncSession) -> User:
+    ur = UserRepository(session)
+    if await ur.get_by_tg_id(tg_id):
+        raise Exception("something 6")
+    user = await ur.create(tg_id)
+    if not user:
+        raise Exception("something 7")
+    return user
+
+
 session_manager = DatabaseSessionManager(
     get_db_url(
         user=settings.db_user,
@@ -60,150 +72,172 @@ session_manager = DatabaseSessionManager(
     ), {"echo": False}
 )
 
+class StaticButton():
+    def __init__(self, text: str, only_for_admin: bool = False, only_for_creator: bool = False) -> None:
+        self.text = text
+        self.only_for_admin = only_for_admin
+        self.only_for_creator = only_for_creator
+
+    text: str
+    only_for_admin: bool
+    only_for_creator: bool
+
+class StaticButtons:
+    start_game = StaticButton("start game")
+    end_game = StaticButton("end game")
+    settings = StaticButton("settings")
+    content = StaticButton("content", only_for_creator=True)
+    creators = StaticButton("content", only_for_admin=True)
+    main_menu = StaticButton("back to main menu")
+
+
+
+""" class Dirs(str, Enum):
+    main_menu = "main_menu"
+    settings = "settings"
+    play = "play"
+    creators = "creators"
+    content = "content"
+
+static_buttons = {
+    Dirs.main_menu.value: ["start game", "settings"],
+} """
+
 router = Router()
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Генерация клавиатур под разные состояния
-# —————————————————————————————————————————————————————————————————————————————
-def main_menu_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Начать игру"), KeyboardButton(text="Настройки"), ]], resize_keyboard=True)
-    return kb
-
-def settings_kb() -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Вернуться в главное меню")]], resize_keyboard=True)
-    return kb
-
-async def playing_kb(answers: List[str]) -> ReplyKeyboardMarkup:
-    buttons:list[list[KeyboardButton]] = [[]]
-    for ans in answers:
-        buttons[0].append(KeyboardButton(text=ans))
-    buttons[0].append(KeyboardButton(text="Закончить игру"))
-    kb = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
-    
-    return kb
+class AppState(StatesGroup):
+    main_menu = State()
+    play = State()
+    settings = State()
+    content = State()
+    creators = State()
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Загрузка/регистрация пользователя
-# —————————————————————————————————————————————————————————————————————————————
-async def get_or_create_user(tg_id: int, session: AsyncSession) -> User:
-    ur = UserRepository(session)
-    user = await ur.get_by_tg_id(tg_id)
-    if not user:
-        user = await ur.create(tg_id)
-    return user
+class GetKeyboardSizeFunction(Protocol):
+    def __call__(self,
+                 values: list[str]
+                 ) -> list[int]:
+        ...
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Заглушки для отсутствующей логики
-# —————————————————————————————————————————————————————————————————————————————
-async def stub_show_settings(message: types.Message, state: FSMContext):
-    # TODO: реализовать отображение и сохранение настроек пользователя
-    await message.answer("Здесь будут настройки (пока заглушка).", reply_markup=settings_kb())
-
-async def stub_process_answer(message: types.Message, state: FSMContext, selected: str):
-    # TODO: ваша логика проверки ответа
-    await message.answer(f"Вы выбрали «{selected}» — пока заглушка обработки ответа.")
-    # после обработки можно отправить следующий вопрос или закончить игру
+def get_keyboard_size(values: list[str]) -> list[int]:
+    length = len(values)
+    res = []
+    for i in range(4, length, 4):
+        res.append(4)
+    res.append(length % 4)
+    return res
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Отправка вопроса в состоянии игры
-# —————————————————————————————————————————————————————————————————————————————
-async def send_question(message: types.Message, state: FSMContext):
+def create_keyboard(values: list[str], keyboard_size: GetKeyboardSizeFunction = get_keyboard_size) -> ReplyKeyboardMarkup:
+    markup = keyboard_size(values)
+    keyboard = []
+    index = 0
+    for i in range(len(values)):
+        for j in range(markup[i]):
+            keyboard.append(KeyboardButton(text=values[index]))
+            index += 1
+
+    return ReplyKeyboardMarkup(keyboard=keyboard)
+
+
+async def send_question(target: types.Message | types.CallbackQuery, state: FSMContext):
     async with session_manager.context_session() as session:
         qr = QuestionRepository(session)
-        current = await qr.get_random_question()
-        if not current:
-            await message.answer("Вопросы закончились, игра окончена.", reply_markup=main_menu_kb())
-            await state.set_state(AppState.main_menu)
-            return
+        current_question = await qr.get_random_question()
+        if not current_question:
+            raise Exception("something 5")
+        questions = await qr.get_random_questions(current_question.answers_count - 1, [current_question])
+        questions.append(current_question)
 
-        # собираем варианты ответов
-        others = await qr.get_random_questions(current.answers_count - 1, [current])
-        options = [x.file.answer for x in others] + [current.file.answer]
-        random.shuffle(options)
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text=item.file.answer,
+                                  callback_data=f"answer:{item.id}")]
+            for item in questions
+        ]
+    )
+    caption = "Guess the film?"
 
-        # сохраняем correct_answer в FSM
-        await state.update_data(correct=current.file.answer)
+    minio_client = get_s3_client()
+    obj = minio_client.get_object(
+        bucket_name=settings.minio_bucket,
+        object_name=str(current_question.file.id)
+    )
+    data = obj.read()
+    obj.close()
+    input_file = BufferedInputFile(
+        data, filename=f"{current_question.file.id}")
 
-        # получаем файл из S3
-        client = get_s3_client()
-        obj = client.get_object(bucket_name=settings.minio_bucket, object_name=str(current.file.id))
-        data = obj.read(); obj.close()
-        input_file = BufferedInputFile(data, filename=str(current.file.id))
+    media_type = current_question.file.file_type.split("/")[0]
+    if isinstance(target, types.CallbackQuery):
+        send_target = target.message
+    else:
+        send_target = target
 
-        media = current.file.file_type.split("/")[0]
-        kb = await playing_kb(options)
+    if not send_target:
+        raise Exception("something 1")
 
-        # отправляем в зависимости от типа
-        if media == "image":
-            await message.answer_photo(photo=input_file, caption="Угадайте фильм:", reply_markup=kb)
-        elif media == "video":
-            await message.answer_video(video=input_file, caption="Угадайте фильм:", reply_markup=kb)
-        elif media == "audio":
-            await message.answer_voice(voice=input_file, caption="Угадайте фильм:", reply_markup=kb)
-        else:
-            await message.answer("Неподдерживаемый тип файла.", reply_markup=kb)
+    match media_type:
+        case "image":
+            await send_target.answer_photo(photo=input_file, caption=caption, reply_markup=keyboard)
+
+        case "video":
+            await send_target.answer_video(video=input_file, caption=caption, reply_markup=keyboard)
+
+        case "audio":
+            await send_target.answer_voice(voice=input_file, caption=caption, reply_markup=keyboard)
+        case _:
+            send_target.answer("internal server error")
+
+    await state.update_data(question_id=current_question.id)
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Стартовые команды /start
-# —————————————————————————————————————————————————————————————————————————————
 @router.message(Command("start"))
-async def cmd_start(message: types.Message, state: FSMContext):
-    await state.set_state(AppState.main_menu)
-    await message.answer("Добро пожаловать! Ваше меню:", reply_markup=main_menu_kb())
+async def cmd_start(message: types.Message):
+    async with session_manager.context_session() as session:
+        if not message.from_user:
+            raise Exception("something 4")
+        user = await get_user(message.from_user.id, session)
+        await message.answer(f"hello, your rank: {user.rank}")
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Общий обработчик всех нажатий ReplyKeyboard
-# —————————————————————————————————————————————————————————————————————————————
 @router.message()
 async def handle_button(message: types.Message, state: FSMContext):
     text = message.text
     current = await state.get_state()
 
-    # главное меню
-    if current == AppState.main_menu.state:
-        match text:
-            case "Начать игру":
-                await state.set_state(AppState.playing)
-                await send_question(message, state)
-            case "Настройки":
-                await state.set_state(AppState.settings)
-                await stub_show_settings(message, state)
-            case _:
-                await message.answer("Пожалуйста, выберите пункт меню.", reply_markup=main_menu_kb())
+    match current:
+        case AppState.main_menu.state:
+            match text:
+                case StaticButtons.start_game.text:
+                    pass
+                case StaticButtons.settings.text:
+                    pass
+                case StaticButtons.content.text:
+                    pass
+                case StaticButtons.creators.text:
+                    pass
+                case _:
+                    pass
+        case AppState.play.state:
+            match text:
+                case StaticButtons.end_game.text:
+                    pass
 
-    # настройки
-    elif current == AppState.settings.state:
-        match text:
-            case "Вернуться в главное меню":
-                await state.set_state(AppState.main_menu)
-                await message.answer("Главное меню:", reply_markup=main_menu_kb())
-            case _:
-                await message.answer("В настройках доступна только кнопка «Вернуться в главное меню».", reply_markup=settings_kb())
-
-    # во время игры
-    elif current == AppState.playing.state:
-        if text == "Закончить игру":
-            await state.set_state(AppState.main_menu)
-            await message.answer("Игра окончена. Главное меню:", reply_markup=main_menu_kb())
-        else:
-            # здесь можно вставить вашу логику из BattleRepository и change_rank
-            await stub_process_answer(message, state, text)
-
-    # прочие состояния
-    else:
-        await state.set_state(AppState.main_menu)
-        await message.answer("Возврат в главное меню.", reply_markup=main_menu_kb())
+                case _:
+                    pass
+        case AppState.settings.state:
+            pass
+        case AppState.content.state:
+            pass
+        case AppState.creators.state:
+            pass
+        case _:
+            pass
 
 
-# —————————————————————————————————————————————————————————————————————————————
-# Запуск бота
-# —————————————————————————————————————————————————————————————————————————————
 async def main():
     bot = Bot(token=settings.tg_bot_token)
     dp = Dispatcher()
